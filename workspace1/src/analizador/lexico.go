3d81e5181cbe17e1d2096f8e4ff91d40b72336aa
Juan Pablo Díaz Correa <juanipis@gmail.com>
2022-07-28 17:35:31 -0500

    Solución problema de dualidad simbolos

--- workspace1/src/analizador/lexico.go
+++ workspace1/src/analizador/lexico.go
@@ -47,32 +47,41 @@ func crearMapa(path string) {
 //Cuando encuentra un separador, lo envia a la función reconocimiento
 //Luego limpia el string temporaeparador o un salto de linea que acabaria la instancia
 func leer(linea string, fila int) {
-	var tempToken string
-	lineaTokens := []token{}
+	var stringAuxToken string
+	lineaEvaluar := []token{}
 
 	for i := 0; i < len(linea); i++ {
-		lineaActual := string(linea[i])
-		typeTokenSeparator, isSeparator := tablaSimbolos[lineaActual]
-		//if,hola//Encontro un separador
+		caracterActual := string(linea[i])
+		tipoSeparador, isSeparator := tablaSimbolos[caracterActual]
+		//++
+		//Algunos simbolos son de dos caracteres, revisemos si el siguiente caracter es un separador en conjunto con el actual
 		if isSeparator {
-			//Revisa si el tempToken acumulado es un simbolo reservado
-			typeTokenTemp, isReserved := tablaSimbolos[tempToken]
+			if i+1 < len(linea) {
+				caracterSiguiente := string(linea[i+1])
+				_, isSeparator := tablaSimbolos[caracterActual+caracterSiguiente]
+				if isSeparator {
+					caracterActual = caracterActual + caracterSiguiente
+					i = i + 2
+				}
+			}
+			//Revisa si el stringAuxToken acumulado es un simbolo de la tabla
+			typeTokenTemp, isReserved := tablaSimbolos[stringAuxToken]
 			if isReserved {
 				//Si es un simbolo reservado, lo añade a la tabla de tokens con su respectivo tipo
-				lineaTokens = append(lineaTokens, token{tempToken, typeTokenTemp})
+				lineaEvaluar = append(lineaEvaluar, token{stringAuxToken, typeTokenTemp})
 			} else {
 				//si no es un simbolo reservado, lo añade a la tabla de tokens con tipo 0==Identificador
-				lineaTokens = append(lineaTokens, token{tempToken, []string{"0"}})
+				lineaEvaluar = append(lineaEvaluar, token{stringAuxToken, []string{"0"}})
 			}
-			//Limpia el tempToken
-			tempToken = ""
+			//Limpia el stringAuxToken
+			stringAuxToken = ""
 			//Añade el separador a la tabla de tokens con su respectivo tipo
-			lineaTokens = append(lineaTokens, token{lineaActual, typeTokenSeparator})
+			lineaEvaluar = append(lineaEvaluar, token{caracterActual, tipoSeparador})
 		} else {
-			//No encontro un separador, lo concatena al tempToken y sigue leyendo
-			tempToken += lineaActual
+			//No encontro un separador, lo concatena al stringAuxToken y sigue leyendo
+			stringAuxToken += caracterActual
 		}
 	}
 	//Al terminar de recorrer la linea añade el conjunto de tokens a la tabla de
-	tablaFinal[fila] = lineaTokens
+	tablaFinal[fila] = lineaEvaluar
 }
