b8ef551002a849ad1ffd93c006d217e9f6c185ae
Juan Pablo Díaz Correa <juanipis@gmail.com>
2022-08-27 14:21:32 -0500

    Boton de abrir archivo y tablas base

--- workspace2/Flutter/analizador_lexicografico/lib/main.dart
+++ workspace2/Flutter/analizador_lexicografico/lib/main.dart
@@ -1,115 +1,189 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Analizador());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Analizador extends StatelessWidget {
+  const Analizador({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "Analizador Lexicografico",
+      home: HomeAnalizador(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeAnalizador extends StatefulWidget {
+  const HomeAnalizador({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeAnalizador> createState() => _HomeAnalizadorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeAnalizadorState extends State<HomeAnalizador> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Analizador Lexicografico"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SizedBox(
+        width: double.infinity,
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 200,
+          runSpacing: 4.0,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Column(
+              children: [const Text("Tabla 1"), tablaLexico()],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Column(
+              children: [const Text("Tabla 2"), tablaTokens()],
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          dialogOpen(context);
+          /*
+          FilePickerResult? result = await FilePicker.platform.pickFiles();
+          if (result != null) {
+            File file = File(result.files.single.path!);
+            print(file.path);
+          } else {}*/
+        },
+        backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+
+  tablaLexico() {
+    return DataTable(columns: const <DataColumn>[
+      DataColumn(label: Text("Nombre")),
+      DataColumn(label: Text("Linea")),
+      DataColumn(label: Text("Columna")),
+      DataColumn(label: Text("Tipo 1")),
+      DataColumn(label: Text("Tipo 2")),
+      DataColumn(label: Text("Tipo 3"))
+    ], rows: const <DataRow>[
+      DataRow(cells: <DataCell>[
+        DataCell(Text("Hola")),
+        DataCell(Text("Mundo")),
+        DataCell(Text("Mundo")),
+        DataCell(Text("Mundo")),
+        DataCell(Text("Mundo")),
+        DataCell(Text("Mundo"))
+      ])
+    ]);
+  }
+
+  tablaTokens() {
+    return DataTable(columns: const <DataColumn>[
+      DataColumn(label: Text("Token")),
+      DataColumn(label: Text("#IdToken")),
+      DataColumn(label: Text("Lexema generador")),
+    ], rows: const <DataRow>[
+      DataRow(cells: <DataCell>[
+        DataCell(Text("Hola")),
+        DataCell(Text("Mundo")),
+        DataCell(Text("Mundo"))
+      ])
+    ]);
+  }
 }
+
+void dialogOpen(BuildContext context) {
+  NavigatorState navState = Navigator.of(context);
+  TextEditingController ruta = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: const Text("Abir archivo"),
+          actions: [
+            Form(
+                key: formKey,
+                child: TextFormField(
+                  controller: ruta,
+                  enabled: true,
+                  decoration:
+                      const InputDecoration(label: Text("Ruta del archivo")),
+                  validator: ((value) {
+                    if (value == null || value == "") {
+                      return "Por favor ingresa una ruta";
+                    } else {
+                      return null;
+                    }
+                  }),
+                )),
+            Row(
+              children: <Widget>[
+                TextButton(
+                    onPressed: () async {
+                      FilePickerResult? result = await FilePicker.platform
+                          .pickFiles(allowedExtensions: ['messi']);
+                      if (result != null) {
+                        File file = File(result.files.single.path!);
+                        ruta.text = file.path;
+                      } else {}
+                    },
+                    child: const Text("Abir")),
+                TextButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        navState.pop();
+                        circularProgress(context);
+                        final isValidRuta = await File(ruta.text).exists();
+                        if (isValidRuta) {
+                          requestApiTables();
+                        } else {
+                          navState.pop();
+                          archivoNoEncontrado(context);
+                        }
+                      }
+                    },
+                    child: const Text("Enviar"))
+              ],
+            )
+          ],
+        );
+      }));
+}
+
+Future<dynamic> archivoNoEncontrado(
+  BuildContext context,
+) {
+  return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: ((context) => const AlertDialog(
+            title: Text("No se encontro el archivo"),
+            actions: [Text("El archivo especificado no ha sido encontrado")],
+          )));
+}
+
+Future<dynamic> circularProgress(BuildContext context) {
+  return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: ((context) => const Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          )));
+}
+
+Future<bool> validarArchivo(String value) async {
+  return await File(value).exists();
+}
+
+void requestApiTables() {}
