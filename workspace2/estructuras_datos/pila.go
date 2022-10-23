aca4567496d5537d8ca14ac21b81c806cb36ff27
Juan Pablo Díaz Correa <juanipis@gmail.com>
2022-10-23 09:32:14 -0500

    Pila en workspace 2

---
+++ workspace2/estructuras_datos/pila.go
@@ -0,0 +1,28 @@
+package estructurasdatos
+
+type stack[T any] struct {
+	Push   func(T)
+	Pop    func() T
+	Length func() int
+	Head   func() T
+}
+
+func Stack[T any]() stack[T] {
+	slice := make([]T, 0)
+	return stack[T]{
+		Push: func(i T) {
+			slice = append(slice, i)
+		},
+		Pop: func() T {
+			res := slice[len(slice)-1]
+			slice = slice[:len(slice)-1]
+			return res
+		},
+		Length: func() int {
+			return len(slice)
+		},
+		Head: func() T {
+			return slice[len(slice)-1]
+		},
+	}
+}
